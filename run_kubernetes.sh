de4bc230b6bab1cbb7a299a57a1c6451c7145b3e
EC2 Default User <ec2-user@ip-172-31-16-23.us-east-2.compute.internal>
2022-07-17 13:23:55 +0000

    Added code change to config.yml and run kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=sreenathvasu/udacitypj4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacitypj4 --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udacitypj4 8000:80
 
